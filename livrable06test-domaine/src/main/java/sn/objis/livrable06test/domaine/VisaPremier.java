8312f0ace22a6a42432640327983cbe6c30da87d
Bilal Fall <bilxadim@gmail.com>
2018-12-29 17:21:53 +0000

    Annulation du test commit + push

--- livrable06test-domaine/src/main/java/sn/objis/livrable06test/domaine/VisaPremier.java
+++ livrable06test-domaine/src/main/java/sn/objis/livrable06test/domaine/VisaPremier.java
@@ -9,10 +9,17 @@ public class VisaPremier extends Carte {
 		// TODO Auto-generated constructor stub
 	}
 
-	
+	public VisaPremier(int numeroCarte, boolean etatCarte, Compte compte, String zone) {
+		super(numeroCarte, etatCarte, compte);
+		this.zone = zone;
+	}
 
-	
-	
-	
+	public String getZone() {
+		return zone;
+	}
+
+	public void setZone(String zone) {
+		this.zone = zone;
+	}
 
 }
